c07d73e8ebb4852d2ba97570b1b5741a85bdc10a
lduynkerke <lduynkerke@gmail.com>
2024-05-30 12:04:45 +0200

    Add load_img()

--- src/blocks.py
+++ src/blocks.py
@@ -6,6 +6,7 @@ from pathlib import Path # Path library
 import numpy as np # numpy library
 import pandas as pd # pandas library
 import seaborn as sns # seaborn library for plotting figures
+import random
 
 import torch # pytorch library
 import torch.nn as nn # neural network module
@@ -143,3 +144,28 @@ class Autoencoder(nn.Module):
         latent = self.encoder.forward(x)
         x_hat = self.decoder.forward(latent)
         return x_hat
+
+
+def load_img(file_path, noise_mean=0, noise_std=0.01):
+    """
+    Load a .npy file, apply a random rotation (90, 180, 270 degrees),
+    and add random Gaussian noise to the data.
+
+    Parameters:
+    file_path (str): Path to the .npy file to be loaded.
+    noise_mean (float): Mean of the Gaussian noise to be added. Default is 0.
+    noise_std (float): Standard deviation of the Gaussian noise to be added. Default is 0.01.
+
+    Returns:
+    tensor_scan (torch.Tensor): The processed image tensor.
+    """
+    scan = np.load(file_path)
+
+    rotate = random.choice([1, 2, 3])
+    scan = np.rot90(scan, k=rotate, axes=(0, 1))
+    noise = np.random.normal(noise_mean, noise_std, scan.shape)
+
+    noisy_scan = scan + noise
+    tensor_scan = torch.tensor(noisy_scan, dtype=torch.float32).unsqueeze(0)
+
+    return tensor_scan
--- src/model.py
+++ src/model.py
@@ -1,12 +1,16 @@
-def mse_loss_fn(pred: torch.Tensor, gt: torch.Tensor):
-      """
-      The MSE error
-      Parameters:
-      pred (torch.Tensor): The reconstructed output from the autoencoder.
-      gt (torch.Tensor): The original input to the autoencoder.
+import numpy as np
+import torch
 
-      Returns:
-      torch.Tensor: The mean squared error loss of the autoencoder
-      """
-      mse = np.mean((pred - gt)**2)
-      return mse
+
+def mse_loss_fn(pred: torch.Tensor, gt: torch.Tensor):
+    """
+    The MSE error
+    Parameters:
+    pred (torch.Tensor): The reconstructed output from the autoencoder.
+    gt (torch.Tensor): The original input to the autoencoder.
+
+    Returns:
+    torch.Tensor: The mean squared error loss of the autoencoder
+    """
+    mse = np.mean((pred - gt) ** 2)
+    return mse
