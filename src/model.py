b6c1d40ad0af762edb4d2ce67fea8f2a2a7f6e88
lduynkerke <lduynkerke@gmail.com>
2024-05-30 11:34:54 +0200

    Add gitignore

--- src/model.py
+++ src/model.py
@@ -8,5 +8,5 @@ def mse_loss_fn(pred: torch.Tensor, gt: torch.Tensor):
       Returns:
       torch.Tensor: The mean squared error loss of the autoencoder
       """
-      mse = (pred - gt)**2
-      return mse.mean()
+      mse = np.mean((pred - gt)**2)
+      return mse
