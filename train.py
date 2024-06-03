9cd4b3f598b1e5e24cf1431ae9d17715c5bc9406
HamdiElsayed <85553581+HamdiElsayed@users.noreply.github.com>
2024-06-03 10:58:26 +0200

    test.ipynb

---
+++ train.py
@@ -0,0 +1,17 @@
+from blocks import ConvEncoder, ConvDecoder,Autoencoder
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+import warnings # ignore future warnings for now
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+from pathlib import Path # Path library
+
+import numpy as np
+import pandas as pd
+import random
+
+
+
