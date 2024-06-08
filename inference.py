444dcc95a804138e3abff162ae504751b5c3f522
lduynkerke <lduynkerke@gmail.com>
2024-06-08 23:55:49 +0200

    Update inference.py so that it takes one folder as input and applies the model

--- inference.py
+++ inference.py
@@ -1,5 +1,37 @@
 import os
 import numpy as np
+import pandas as pd
+import torch
+from torchvision import transforms
+from src.blocks import Autoencoder, ConvDecoder, ConvEncoder
+
+
+def load_model(weights_path, encoder, decoder, device='cpu'):
+    model = Autoencoder(encoder, decoder)
+    map_location = torch.device(device) if torch.cuda.is_available() and 'cuda' in device else torch.device('cpu')
+    model.load_state_dict(torch.load(weights_path, map_location=map_location))
+    model.to(map_location)
+    model.eval()
+    return model
+
+
+def preprocess_image(image_array):
+    if image_array.dtype != np.float32:
+        image_array = image_array.astype(np.float32)
+    transform = transforms.Compose([
+        transforms.ToTensor()
+    ])
+    image_tensor = transform(image_array)
+    image_tensor = image_tensor.unsqueeze(0)  # Add batch dimension
+    return image_tensor
+
+
+def get_prediction(model, image_tensor, device):
+    model.to(device)
+    image_tensor = image_tensor.to(device)
+    with torch.no_grad():
+        output = model(image_tensor)
+    return output.cpu().numpy()
 
 
 def calculate_mse(img1, img2):
@@ -50,16 +82,35 @@ def evaluate_images(original_folder, reconstructed_folder, max_mse):
 
 
 def main():
-    original_folder = 'original_2d'
-    reconstructed_folder = 'reconstructed_2d'
+    weights = "best_model.pth"
+    encoder = ConvEncoder(num_channels=64, kernel_size=5, strides=1, pooling=2)
+    decoder = ConvDecoder(num_channels=64, kernel_size=5, strides=2)
+    model = load_model(weights, encoder, decoder)
     max_mse = 1024 ** 2
+    folder = "data_2d"
 
-    mse_list, similarity_list = evaluate_images(original_folder, reconstructed_folder, max_mse)
+    # mse_list, similarity_list = evaluate_images("data_2d", reconstructed_folder, max_mse)
+
+    file_names = sorted([f for f in os.listdir(folder) if f.endswith('.npy')])
+    mse_list, similarity_list = [], []
+    for file_name in file_names:
+        image_array = np.load(os.path.join(folder, file_name))  # Load your image array here
+        image_tensor = preprocess_image(image_array)
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        prediction = get_prediction(model, image_tensor, device)
+
+        mse = calculate_mse(image_array, prediction)
+        similarity = calculate_similarity(mse, max_mse)
+        mse_list.append(mse)
+        similarity_list.append(similarity)
 
     print("Evaluation Results:")
     print(f"Mean MSE: {np.mean(mse_list)}")
     print(f"Mean Similarity: {np.mean(similarity_list)}%")
     print(f"Individual Similarity values: {similarity_list}")
+
+    model_results = pd.DataFrame({"file": file_names, "mse": mse_list, "similarity": similarity_list})
+    model_results.to_csv('model_results.csv', index=False)
 
 
 if __name__ == "__main__":
